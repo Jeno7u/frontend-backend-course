97c919650196acd3e974d4f4506f1ebfb49f20a0
Mironov Boris <lboris201@yandex.ru>
2025-09-22 13:10:35 +0300

    Nevermind

---
+++ scripts/main.js
@@ -0,0 +1,45 @@
+const dlg = document.getElementById("contactDialog");
+const openBtn = document.getElementById("openDialog");
+const closeBtn = document.getElementById("closeDialog");
+const form = document.getElementById("contactForm");
+let lastActive = null;
+
+openBtn.addEventListener("click", () => {
+  lastActive = document.activeElement;
+  dlg.showModal(); // модальный режим +
+  dlg.querySelector("input,select,textarea,button")?.focus();
+});
+
+closeBtn.addEventListener("click", () => dlg.close("cancel"));
+
+form?.addEventListener("submit", (e) => {
+  // 1) Сброс кастомных сообщений
+  [...form.elements].forEach((el) => el.setCustomValidity?.(""));
+  // 2) Проверка встроенных ограничений
+  if (false) {
+    //!form.checkValidity()
+    e.preventDefault();
+    // Пример: таргетированное сообщение
+    const email = form.elements.email;
+    if (email?.validity.typeMismatch) {
+      email.setCustomValidity(
+        "Введите корректный e-mail, например name@example.com"
+      );
+    }
+    form.reportValidity();
+    [...form.elements].forEach((el) => {
+      if (el.willValidate)
+        el.toggleAttribute("aria-invalid", !el.checkValidity());
+    });
+    return;
+  }
+  e.preventDefault();
+  document.getElementById("contactDialog")?.close("success");
+  form.reset();
+  window.location.href = "thanks.html";
+});
+
+dlg.addEventListener("close", () => {
+  lastActive.focus();
+});
+// Esc по умолчанию вызывает событие 'cancel' и закрывает <dialog>
